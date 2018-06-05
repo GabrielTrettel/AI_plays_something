8fc351c5f44ce73ef7f14d4e25b004db4fc55d09
Gabriel Trettel <gabrielmtrettel@gmail.com>
2018-06-04 23:55:46 -0300

    Add new functions and fit model

---
+++ DinoRunner/balance_data.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from collections import Counter
+from random import shuffle
+import cv2
+
+
+def balance_data(verbose=True, train_file='training_data.npy'):
+	train_data = np.load(train_file)
+
+	# uncomment the following lines if you want to see the video and controls record
+	#
+	# for data in train_data:
+	# 	img = data[0]
+	# 	choice = data[1]
+	# 	cv2.imshow('test', img)
+	# 	print(choice)
+
+	# 	if cv2.waitKey(25) & 0xFF == ord('q'):
+	# 		cv2.destroyAllWindows()
+	# 		break
+
+
+	df = pd.DataFrame(train_data)
+
+	ups = []
+	downs = []
+	walks = []
+
+	shuffle(train_data)
+
+	for data in train_data:
+		img = data[0]
+		choice = data[1]
+
+		if choice == [0,0]:
+			walks.append([img, choice])
+
+		elif choice == [1,0]:
+			ups.append([img, choice])
+
+		elif choice == [0,1]:
+			downs.append([img, choice])
+
+		else:
+			print("No matches! Something went wrong")
+
+
+	walks = walks[:len(ups)]
+	downs = downs[:len(ups)]
+	ups   = ups[:len(ups)]
+
+	final_data = walks + ups + downs
+
+	shuffle(final_data)
+
+	save_file = "balanced_" + train_file
+
+	np.save(save_file, final_data)
+
+	if verbose:
+		print("Initial train data len: ", len(train_data))
+		print("Final train data len: ", len(final_data))
+		print(df.head())
+		print(Counter(df[1].apply(str)))
+
+	return save_file
--- DinoRunner/main.py
+++ DinoRunner/main.py
@@ -1,18 +1,21 @@
 from DinoEye import screen_record
 from DinoEye import keyboard_record
+from DinoBrain import train_skills
+from balance_data import balance_data
 import numpy as np
 import os
 import cv2
+
 
 class Dino:
 	# def __init__(self):
 		# self.training_file_name = training_file_name
 
 
-	def createTrainFile(self, training_file_name="training_data.npy"):
-		if os.path.isfile(training_file_name):
+	def createTrainFile(self, training_file="training_data.npy"):
+		if os.path.isfile(training_file):
 			print('File exists, loading previous data!')
-			training_data = list(np.load(training_file_name))
+			training_data = list(np.load(training_file))
 		else:
 			print('File does not exist, starting fresh')
 			training_data = []
@@ -29,7 +32,7 @@ class Dino:
 
 			if len(training_data) % 500 == 0:
 				print(len(training_data))
-				np.save(training_file_name, training_data)
+				np.save(training_file, training_data)
 
 			cv2.imshow('window', image)
 			if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -37,7 +40,20 @@ class Dino:
 				break				
 
 
+	def trainJumpSkill(self, training_data="balanced_training_data.npy"):
+		jump_skill = train_skills()
 
 
-dino = Dino()
-dino.createTrainFile()
+
+if __name__ == '__main__':
+	
+	train_file = "training_data2.npy"
+	dino = Dino()
+
+	# dino.createTrainFile(training_file=train_file)
+
+	balanced_file = "balanced_training_data.npy"
+	# balanced_file = balance_data(train_file)
+
+	dino.trainJumpSkill(training_data=balanced_file)
+
