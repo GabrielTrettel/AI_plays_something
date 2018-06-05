5a780b180ccc99cea9b820f90abc688014efaa33
Gabriel Trettel <gabrielmtrettel@gmail.com>
2018-06-05 00:02:53 -0300

    Fixed a typo

--- DinoRunner/DinoBrain.py
+++ DinoRunner/DinoBrain.py
@@ -7,12 +7,11 @@ def train_skills(training_data='balanced_training_data.npy'):
 	HEIGHT = 16
 	LR = 1e-3
 	EPOCHS = 8
-	MODEL_NAME = 'pyDinoRunner3-{}-{}-{}-epochs.model'.format(LR, 'alexnetv2', EPOCHS)
+	MODEL_NAME = 'pyDinoRunner-{}-{}-{}-epochs.model'.format(LR, 'alexnetv2', EPOCHS)
 
 	model = alexnet(WIDTH, HEIGHT, LR)
 
 	train_data = np.load(training_data)
-
 
 	pivo = int(0.1 * len(train_data))
 	train = train_data[:-pivo]
@@ -20,14 +19,10 @@ def train_skills(training_data='balanced_training_data.npy'):
 
 	X = np.array([i[0] for i in train]).reshape(-1, WIDTH, HEIGHT, 1)
 	Y = np.array([i[1] for i in train])
-	Y = Y.reshape(len(Y), 2)
 
-	print(len(X), len(Y))
 
 	test_x = np.array([i[0] for i in test]).reshape(-1, WIDTH, HEIGHT, 1)
 	test_y = np.array([i[1] for i in test])
-	test_y = test_y.reshape(len(test_y), 2)
-	# print(Y)
 
 	model.fit({'input': X}, {'targets': Y}, n_epoch=EPOCHS, validation_set=({'input': test_x}, {'targets': test_y}), 
     snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
