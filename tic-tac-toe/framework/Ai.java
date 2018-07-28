ceff359270f86b3a78cd06f2bbc04c8158a2c947
Gabriel Trettel <gabrielmtrettel@gmail.com>
2018-07-28 12:06:26 -0300

    Tic-tac-toe: AI difficulty level created

--- tic-tac-toe/framework/Ai.java
+++ tic-tac-toe/framework/Ai.java
@@ -1,21 +1,54 @@
+import java.util.Random;
+
 public class Ai extends Player {
+    // this.level pertence ao intervalo [0, 3]
+    private int level;
+    private Random generator;
+
     public Ai() {
         this.is_ai = true;
         this.name = "Chuck Norris";
+        this.level = 0;
+        this.generator = new Random();
     }
     // Sobrescrita de método para se aproveitar do polimorfismo de player e Ai
-    //@Overide
-    public int[] getMove(Board board) {
-        int[] moves = thinkBestMove(board);
-        return moves;
-    }
-    //@Overide
+    @Override
     public void setName() {
         //TODO
         // Scanner sc1 = new Scanner(System.in);
         // System.out.printf("Escolha com quem você quer jogar %d: ", player_id);
         // name = sc1.nextLine();
+
+    @Override
+    public int[] getMove(Board board) {
+        float probability = this.generator.nextFloat();
+        int[] moves;
+
+        if(this.level > probability || this.level == 20)
+            moves = board.getFreeCells()[ this.generator.nextInt( board.getFreeSpace()-1 ) ];
+        else
+            moves = thinkBestMove(board);
+
+
+        return moves;
+
     }
+
+}
+
+    public void setLevel(int x) {
+        int lvl;
+        Scanner sc1 = new Scanner(System.in);
+
+        do {
+            System.out.printf("Escolha o nível que voce quer jogar \n 0 -  Impossível  |  1 - Dificil  |  2 - Médio  |  3 - Fácil  |  4 - Modo hu3");
+            lvl = sc1.nextInt();
+            System.out.print("\33[1A\33[2K");
+        } while(lvl < 0 || lvl > 4);
+
+        this.level = lvl*5;
+    }
+
     private int[] thinkBestMove(Board board) {
         int[][] avaliableCells = board.getFreeCells();
         int[] best_move = {-1, -1};
